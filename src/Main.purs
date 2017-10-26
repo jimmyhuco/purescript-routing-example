db7da6299455e1a47077b202dd552dd9a983bbc7
Jimmy Hu <jimmyhu.co@gmail.com>
2017-10-26 19:01:13 +0800

    more elegant

--- src/Main.purs
+++ src/Main.purs
@@ -3,14 +3,14 @@ module Main where
 import BigPrelude
 
 import Control.Monad.Aff (forkAff)
-
+import Control.Monad.Aff.Console (CONSOLE, log)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
-
 import Router as R
 
-main :: forall eff. Eff (HA.HalogenEffects eff) Unit
+main :: forall eff. Eff (HA.HalogenEffects (console :: CONSOLE | eff)) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
   driver <- runUI R.ui unit body
-  forkAff $ R.routeSignal driver
+  _ <- forkAff $ R.routeSignal driver
+  forkAff $ log "fork log in main"
--- src/Router.purs
+++ src/Router.purs
@@ -1,20 +1,22 @@
 module Router where
 
 import BigPrelude
+
+import Component.Profile as Profile
+import Component.Sessions as Sessions
+import Control.Monad.Aff (Aff)
+import Control.Monad.Aff.Console (log, CONSOLE)
+import Control.Monad.State.Class (put)
+import Data.Functor.Coproduct (Coproduct)
+import Data.String (toLower)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.Component.ChildPath (ChildPath, cp1, cpL, cpR)
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 import Routing (matchesAff)
 import Routing.Match (Match)
 import Routing.Match.Class (lit, num)
-import Component.Profile as Profile
-import Component.Sessions as Sessions
-import Halogen as H
-import Halogen.Aff as HA
-import Halogen.HTML as HH
-import Halogen.HTML.Properties as HP
-import Control.Monad.Aff (Aff)
-import Control.Monad.State.Class (modify)
-import Data.Functor.Coproduct (Coproduct)
-import Data.String (toLower)
-import Halogen.Component.ChildPath (ChildPath, cpR, cpL)
 
 data Input a
   = Goto Routes a
@@ -28,8 +30,16 @@ data Routes
   | Sessions CRUD
   | Home
 
+instance showRoutes :: Show Routes where
+  show Profile = "Profile"
+  show (Sessions Index) = "Sessions"
+  show (Sessions (Show n)) = "Sessions > " <> show n
+  show Home = "Home"
+
+
 init :: State
-init = { currentPage: "Home" }
+-- init = { currentPage: "Home" }
+init = Home
 
 routing :: Match Routes
 routing = profile
@@ -42,9 +52,11 @@ routing = profile
     route str = lit "" *> lit str
     parseCRUD = Show <$> num <|> pure Index
 
-type State =
-  { currentPage :: String
-  }
+-- type State =
+--   { currentPage :: String
+--   }
+
+type State = Routes
 
 type ChildQuery = Coproduct Profile.Input Sessions.Input
 type ChildSlot = Either Profile.Slot Sessions.Slot
@@ -69,39 +81,45 @@ ui = H.parentComponent
     render :: State -> H.ParentHTML Input ChildQuery ChildSlot m
     render st =
       HH.div_
-        [ HH.h1_ [ HH.text (st.currentPage) ]
+        [ HH.h1_ [ HH.text (show st) ]
         , HH.ul_ (map link ["Sessions", "Profile", "Home"])
-        , viewPage st.currentPage
+        , viewPage st
         ]
 
     link s = HH.li_ [ HH.a [ HP.href ("#/" <> toLower s) ] [ HH.text s ] ]
 
-    viewPage :: String -> H.ParentHTML Input ChildQuery ChildSlot m
-    viewPage "Sessions" =
+    viewPage :: Routes -> H.ParentHTML Input ChildQuery ChildSlot m
+    viewPage (Sessions Index) =
       HH.slot' pathToSessions Sessions.Slot Sessions.ui unit absurd
-    viewPage "Profile" =
+    viewPage (Sessions (Show n)) =
+      HH.slot' pathToSessions Sessions.Slot Sessions.ui unit absurd
+    viewPage Profile =
       HH.slot' pathToProfile Profile.Slot Profile.ui unit absurd
     viewPage _ =
       HH.div_ []
 
     eval :: Input ~> H.ParentDSL State Input ChildQuery ChildSlot Void m
-    eval (Goto Profile next) = do
-      modify (_ { currentPage = "Profile" })
+    eval (Goto r next) = do
+      put r
       pure next
-    eval (Goto (Sessions view) next) = do
-      modify case view of
-                  Index -> (_ { currentPage = "Sessions" })
-                  Show n -> (_ { currentPage = "Session " <> show n })
-      pure next
-    eval (Goto Home next) = do
-      modify (_ { currentPage = "Home" })
-      pure next
+    -- eval (Goto (Sessions view) next) = do
+    --   modify case view of
+    --               Index -> (_ { currentPage = "Sessions" })
+    --               Show n -> (_ { currentPage = "Session " <> show n })
+    --   pure next
+    -- eval (Goto Home next) = do
+    --   modify (_ { currentPage = "Home" })
+    --   pure next
+testEffects :: forall eff . String -> Aff (HA.HalogenEffects (console :: CONSOLE | eff)) Unit
+testEffects str = do
+  log str
 
-routeSignal :: forall eff. H.HalogenIO Input Void (Aff (HA.HalogenEffects eff))
-            -> Aff (HA.HalogenEffects eff) Unit
+routeSignal :: forall eff. H.HalogenIO Input Void (Aff (HA.HalogenEffects  (console :: CONSOLE|eff)))
+            -> Aff (HA.HalogenEffects (console :: CONSOLE|eff)) Unit
 routeSignal driver = do
   Tuple old new <- matchesAff routing
   redirects driver old new
+  log "test log in two effects"
 
 redirects :: forall eff. H.HalogenIO Input Void (Aff (HA.HalogenEffects eff))
           -> Maybe Routes
